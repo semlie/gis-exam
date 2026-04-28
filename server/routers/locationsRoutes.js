1f74e4b15fbf5bc1e618b239a05671964bd873e4
sem <israellieb@gmail.com>
2026-04-28 21:25:42 +0300

    feat: add authentication middleware for getAllFarStudents route to restrict access to teachers only

--- server/routers/locationsRoutes.js
+++ server/routers/locationsRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import locationsController from "../controllers/locationsController.js";
+import { isTeacher } from "../middleware/auth.js";
 const locationsRouter = express.Router();
 locationsRouter.get("/getAllLocations", locationsController.getAllLocations);
 locationsRouter.post("/addLocations", locationsController.addLocations);
 locationsRouter.get("/getLocationsByClassId", locationsController.getLocationsByClassId);
 locationsRouter.get("/getLocationsByClassId/:class_id", locationsController.getLocationsByClassId);
-locationsRouter.get("/getAllFarStudents/:class_id/:user_id", locationsController.getAllFarStudents);
+locationsRouter.get("/getAllFarStudents/:class_id/:user_id", isTeacher ,locationsController.getAllFarStudents);
 export default locationsRouter;
